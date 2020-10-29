42f4d6e287b73bc21ffb141e988e338c4256fa65
Vlad Bokov <vlad@lunatic.cat>
2020-10-29 06:35:14 +0300

    Implement query rewrites

--- README.md
+++ README.md
@@ -2,12 +2,37 @@
 
 ![](https://github.com/lunatic-cat/pg_exec_array_params/workflows/ci/badge.svg)
 
+## Usage
+
+```ruby
+# `params` CAN include primitive homogeneous arrays: [[1, 2]]
+PgExecArrayParams.exec_array_params(conn, sql, params)
+```
+
 ## Problem
 
 ```ruby
-pg_connection.exec_params('select * from users where id in ($1)', [1,2])
+pg_connection.exec_params('select * from users where id = $1', [1,2])
 PG::IndeterminateDatatype: ERROR:  could not determine data type of parameter $2
 
-pg_connection.exec_params('select * from users where id in ($1)', [[1,2]])
+pg_connection.exec_params('select * from users where id = $1', [[1,2]])
 PG::InvalidTextRepresentation: ERROR:  invalid input syntax for integer: "[1, 2]"
+```
+
+## Solution
+
+```ruby
+# rewrite query under the hood to
+# select * from users where id IN ($1, $2)
+PgExecArrayParams.exec_array_params(conn, 'select * from users where id = $1', [[1,2]])
+=> [{"id" => 1}, {"id" => 2}]
+```
+
+## Integration with 'pg' gem
+
+```ruby
+PG::Connection.include(PgExecArrayParams) # once in initializer
+
+conn.pg_exec_array_params('select * from users where id = $1', [[1,2]])
+=> [{"id" => 1}, {"id" => 2}]
 ```
--- spec/pg_exec_array_params_spec.rb
+++ spec/pg_exec_array_params_spec.rb
@@ -3,13 +3,46 @@
 require 'spec_helper'
 
 RSpec.describe PgExecArrayParams, :pg do
-  include_context 'shared users table'
+  include_context 'shared tables'
+
+  let(:sql) { 'select * from users where age = $1 order by age' }
+  let(:min_row) { { 'age' => min_age.to_s } }
+  let(:max_row) { { 'age' => max_age.to_s } }
+
+  RSpec.shared_examples 'replacement handler' do
+    it 'working like usual' do
+      expect(exec_array_params(conn, sql, [min_age])).to fetch_rows [min_row]
+    end
+
+    it 'working with array' do
+      expect(exec_array_params(conn, sql, [[min_age, max_age]])).to fetch_rows [min_row, max_row]
+    end
+  end
 
   describe '#exec_array_params' do
-    it 'works with pg' do
-      expect(exec_array_params(
-               conn, 'select * from users where age in ($1)', [min_age]
-             )).to fetch_rows [{ 'age' => min_age.to_s }]
+    describe 'with = $1' do
+      it_behaves_like 'replacement handler'
+    end
+
+    describe 'with IN ($1)' do
+      let(:sql) { 'select * from users where age in ($1) order by age' }
+      it_behaves_like 'replacement handler'
+    end
+
+    describe 'with IN ($1, $2)' do
+      let(:sql) { 'select * from users where age in ($1, $2) order by age' }
+
+      it 'raises error to leave one ref' do
+        expect do
+          exec_array_params(conn, sql, [[min_age, max_row]])
+        end.to raise_error(PgExecArrayParams::Error)
+      end
+    end
+  end
+
+  describe '#included' do
+    it 'works like #exec_array_params' do
+      expect(conn.pg_exec_array_params(sql, [min_age])).to fetch_rows [min_row]
     end
   end
 end
